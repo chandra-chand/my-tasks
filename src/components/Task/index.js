0e9bc43afae61784b195fad065c1cdacda076504
chandu <chandrasekhardamarla24@gmail.com>
2023-01-25 08:53:50 +0000

    first commit

---
+++ src/components/Task/index.js
@@ -0,0 +1,15 @@
+import {TaskName, TagsCategory, ListItem} from './style'
+
+const Task = props => {
+  const {taskDetails} = props
+  const {taskName, taskCategory} = taskDetails
+
+  return (
+    <ListItem>
+      <TaskName>{taskName}</TaskName>
+      <TagsCategory>{taskCategory}</TagsCategory>
+    </ListItem>
+  )
+}
+
+export default Task
